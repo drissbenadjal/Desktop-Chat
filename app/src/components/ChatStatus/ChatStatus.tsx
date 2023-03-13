5ae786787a3027d952902703ffc9d587d694ca5b
drissbenadjal <driss.benadjal@hotmail.com>
2023-03-13 11:34:30 +0100

    update style add icons and ChatStatus for UtilsBar and fix sidebar

---
+++ app/src/components/ChatStatus/ChatStatus.tsx
@@ -0,0 +1,53 @@
+import "./ChatStatus.scss";
+
+import SettingsIcon from "../../assets/icons/setting.svg";
+import MicIcon from "../../assets/icons/mic.svg";
+import MicOffIcon from "../../assets/icons/mic-slash.svg";
+import HeadphoneIcon from "../../assets/icons/headset-mic.svg";
+import HeadphoneOffIcon from "../../assets/icons/headset-mic-slash.svg";
+
+export const ChatStatus = () => {
+  return (
+    <div className="chat-status">
+      <div className="chat-status__container">
+        <div className="chat-status__container__top"></div>
+        <div className="chat-status__container__bottom">
+          <div className="chat-status__container__bottom__left">
+            <button>
+              <div className="status-container">
+                <img
+                  src="https://media.valorant-api.com/playercards/aeb4ce01-4781-9330-43cb-b991d1122c99/smallart.png"
+                  alt="logo"
+                />
+                <div className="status online"></div>
+              </div>
+              <div className="user-infos">
+                <p className="username">Username</p>
+                <p className="tag">#1234</p>
+              </div>
+            </button>
+          </div>
+          <div className="chat-status__container__bottom__right">
+            <ul>
+              <li>
+                <button>
+                    <img src={MicIcon} alt="" />
+                </button>
+              </li>
+              <li>
+                <button>
+                    <img src={HeadphoneIcon} alt="" />
+                </button>
+              </li>
+              <li>
+                <button>
+                    <img src={SettingsIcon} alt="" />
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/src/components/UtilsBar/UtilsBar.tsx
+++ app/src/components/UtilsBar/UtilsBar.tsx
@@ -1,73 +1,103 @@
 import { Link, useLocation } from "react-router-dom";
 
-import CloseIcon from '../../assets/icons/close-svg.svg';
+import "./UtilsBar.scss";
 
-import './UtilsBar.scss';
+import { ChatStatus } from "../ChatStatus/ChatStatus";
+import CloseIcon from "../../assets/icons/close-svg.svg";
 
 export const UtilsBar = ({ infos }: any) => {
-    if(infos === "friends") {
+  if (infos === "friends") {
     return (
-        <div className="utilsbar">
-            <div className="utilsbar-header">
-                <form action="">
-                    <input type="text" placeholder="Search" />
-                </form>
+      <div className="utilsbar">
+      <div className="utilsbar-header">
+        <form action="">
+          <input type="text" placeholder="Search" />
+        </form>
+      </div>
+        <div className="utilsbar-container">
+          <div className="utilsbar-action">
+            <Link
+              to="/home"
+              className={
+                useLocation().pathname === "/home" ||
+                useLocation().pathname === "/"
+                  ? "active"
+                  : ""
+              }
+            >
+              <p>Friends</p>
+            </Link>
+            <Link
+              to="/Premium"
+              className={useLocation().pathname === "/premium" ? "active" : ""}
+            >
+              <p>Premium</p>
+            </Link>
+          </div>
+          <div className="utilsbar-friend">
+            <div className="utilsbar-friend__top">
+              <ul>
+                <li>
+                  <p>MESSAGES PRIVÉS</p>
+                </li>
+                <li>
+                  <button>+</button>
+                </li>
+              </ul>
             </div>
-            <div className="utilsbar-action">
-                <Link to="/home" className={useLocation().pathname === "/home" || useLocation().pathname === "/" ? "active" : ""}>
-                    <p>Friends</p>
-                </Link>
-                <Link to="/Premium" className={useLocation().pathname === "/premium" ? "active" : ""}>
-                    <p>Premium</p>
-                </Link>
+            <div className="utilsbar-friend__bottom">
+              <ul>
+                <li
+                  className={
+                    useLocation().pathname === "/private/1" ? "active-pm" : ""
+                  }
+                >
+                  <Link to="/private/1">
+                    <div className="status-container">
+                      <img
+                        src="https://media.valorant-api.com/playercards/aeb4ce01-4781-9330-43cb-b991d1122c99/smallart.png"
+                        alt="logo"
+                      />
+                      <div className="status dnd"></div>
+                    </div>
+                    <p>Username</p>
+                  </Link>
+                  <button className="close-pm">
+                    <img src={CloseIcon} alt="" />
+                  </button>
+                </li>
+                <li
+                  className={
+                    useLocation().pathname === "/private/2" ? "active-pm" : ""
+                  }
+                >
+                  <Link to="/private/2">
+                    <div className="status-container">
+                      <img
+                        src="https://media.valorant-api.com/playercards/aeb4ce01-4781-9330-43cb-b991d1122c99/smallart.png"
+                        alt="logo"
+                      />
+                      <div className="status online"></div>
+                    </div>
+                    <p>Username</p>
+                  </Link>
+                  <button className="close-pm">
+                    <img src={CloseIcon} alt="" />
+                  </button>
+                </li>
+              </ul>
             </div>
-            <div className="utilsbar-friend">
-                <div className='utilsbar-friend__top'>
-                    <ul>
-                        <li>
-                            <p>MESSAGES PRIVÉS</p>
-                        </li>
-                        <li>
-                            <button>+</button>
-                        </li>
-                    </ul>
-                </div>
-                <div className='utilsbar-friend__bottom'>
-                    <ul>
-                        <li className={useLocation().pathname === "/private/1" ? "active-pm" : ""}>
-                            <Link to="/private/1">
-                                <div className='status-container'>
-                                    <img src="https://media.valorant-api.com/playercards/aeb4ce01-4781-9330-43cb-b991d1122c99/smallart.png" alt="logo" />
-                                    <div className='status dnd'></div>
-                                </div>
-                                <p>Username</p>
-                            </Link>
-                            <button className='close-pm'>
-                                <img src={CloseIcon} alt="" />
-                            </button>
-                        </li>
-                        <li className={useLocation().pathname === "/private/2" ? "active-pm" : ""}>
-                            <Link to="/private/2">
-                                <div className='status-container'>
-                                    <img src="https://media.valorant-api.com/playercards/aeb4ce01-4781-9330-43cb-b991d1122c99/smallart.png" alt="logo" />
-                                    <div className='status online'></div>
-                                </div>
-                                <p>Username</p>
-                            </Link>
-                            <button className='close-pm'>
-                                <img src={CloseIcon} alt="" />
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+          </div>
         </div>
+        <ChatStatus />
+      </div>
     );
-    } else if(infos === "server") {
-        return (
-            <div className="utilsbar">
-                <h1>Server</h1>
-            </div>
-        );
-    }
+  } else if (infos === "server") {
+    return (
+      <div className="utilsbar">
+        <h1>Server</h1>
+        <ChatStatus />
+      </div>
+    );
+  }
 };
