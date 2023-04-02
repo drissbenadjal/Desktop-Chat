056ab3ec800789a3b0e0778420ca657a8ac57568
drissbenadjal <driss.benadjal@hotmail.com>
2023-04-02 20:09:04 +0200

    Change cookie by localStorage

--- app/src/Utils/utilsCookies.tsx
+++ app/src/Utils/utilsCookies.tsx
@@ -1,23 +1,13 @@
 const addCookie = (name: string, value: string, days: number) => {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/`;
+    localStorage.setItem(name, value);
 };
 
 const getCookie = (name: string) => {
-    const cookieName = `${name}=`;
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-        let cookie = cookies[i].trim();
-        if (cookie.indexOf(cookieName) === 0) {
-            return cookie.substring(cookieName.length, cookie.length);
-        }
-    }
-    return null;
+    return localStorage.getItem(name);
 };
 
 const removeCookie = (name: string) => {
-    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+    localStorage.removeItem(name);
 }
 
 export { addCookie, getCookie, removeCookie };
